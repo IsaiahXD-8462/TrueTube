9bf85c9bcbb3deea522a3fcbb07b3d4fec8c2110
IsaiahXD-8462 <116024098+IsaiahXD-8462@users.noreply.github.com>
2023-05-04 20:55:34 -0400

    Added truetube project files

---
+++ Backend/truetube/local_settings.py
@@ -0,0 +1,12 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-^@cf*!vxw=n@al7w=hf$dt&hvrmh34)7*=d#edq8t3o$l_)^5v'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'truetube_database',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'My$&1@1993'
+    }
+}
