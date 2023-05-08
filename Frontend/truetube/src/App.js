e92dfd0003f1b3d3ebe56821e14ff9c9c864ec5c
IsaiahXD-8462 <116024098+IsaiahXD-8462@users.noreply.github.com>
2023-05-08 16:35:38 -0400

    Installed axios

--- Frontend/truetube/src/App.js
+++ Frontend/truetube/src/App.js
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  
+  const [videos, uploadVideos] = useState ([]);
+  
   return (
     <div>
 
